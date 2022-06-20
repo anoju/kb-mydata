46f6f467e3b0785574c58e09c8da472f08c395c7
zun5761 <zun5761@gmail.com>
2022-06-20 18:02:29 +0900

    matchMotion

--- front/js/ui-front.js
+++ front/js/ui-front.js
@@ -5485,6 +5485,7 @@ ui.Confetti = {
 
     if ($wrap.hasClass('type1')) $itemLength = 24;
     // if($wrap.hasClass('type2'))$itemLength = 12;
+    if ($wrap.data('length')) $itemLength = $wrap.data('length');
 
     for (let i = 0; i < $itemLength; i++) {
       rdClass = randomNumber(1, 3, 0);
@@ -8033,6 +8034,156 @@ const Conffeti = {
       }
     });
   }
+};
+
+const canvasLightning = function (c, cw, ch) {
+  // Initialize
+  this.init = function () {
+    this.loop();
+  };
+
+  // Variables
+  const _this = this;
+  this.c = c;
+  this.ctx = c.getContext('2d');
+  this.cw = cw;
+  this.ch = ch;
+  this.mx = 0;
+  this.my = 0;
+  this.lightning = [];
+  this.lightTimeCurrent = 0;
+  this.lightTimeTotal = 50;
+
+  // Utility Functions
+  this.rand = function (rMi, rMa) {
+    return ~~(Math.random() * (rMa - rMi + 1) + rMi);
+  };
+  this.hitTest = function (x1, y1, w1, h1, x2, y2, w2, h2) {
+    return !(x1 + w1 < x2 || x2 + w2 < x1 || y1 + h1 < y2 || y2 + h2 < y1);
+  };
+
+  // Create Lightning
+  this.createL = function (x, y, canSpawn) {
+    this.lightning.push({
+      x: x,
+      y: y,
+      xRange: this.rand(5, 30),
+      yRange: this.rand(5, 25),
+      path: [
+        {
+          x: x,
+          y: y
+        }
+      ],
+      pathLimit: this.rand(10, 35),
+      canSpawn: canSpawn,
+      hasFired: false
+    });
+  };
+
+  // Update Lightning
+  this.updateL = function () {
+    let i = this.lightning.length;
+    while (i--) {
+      const light = this.lightning[i];
+      light.path.push({
+        x: light.path[light.path.length - 1].x + (this.rand(0, light.xRange) - light.xRange / 2),
+        y: light.path[light.path.length - 1].y + this.rand(0, light.yRange)
+      });
+      if (light.path.length > light.pathLimit) {
+        this.lightning.splice(i, 1);
+      }
+      light.hasFired = true;
+    }
+  };
+
+  // Render Lightning
+  this.renderL = function () {
+    let i = this.lightning.length;
+    while (i--) {
+      const light = this.lightning[i];
+      this.ctx.strokeStyle = 'rgba(255, 255, 255, ' + this.rand(10, 100) / 100 + ')';
+      this.ctx.lineWidth = 1;
+      if (this.rand(0, 30) == 0) {
+        this.ctx.lineWidth = 2;
+      }
+      if (this.rand(0, 60) == 0) {
+        this.ctx.lineWidth = 3;
+      }
+      if (this.rand(0, 90) == 0) {
+        this.ctx.lineWidth = 4;
+      }
+      if (this.rand(0, 120) == 0) {
+        this.ctx.lineWidth = 5;
+      }
+      if (this.rand(0, 150) == 0) {
+        this.ctx.lineWidth = 6;
+      }
+      this.ctx.beginPath();
+
+      const pathCount = light.path.length;
+      this.ctx.moveTo(light.x, light.y);
+      for (let pc = 0; pc < pathCount; pc++) {
+        this.ctx.lineTo(light.path[pc].x, light.path[pc].y);
+        if (light.canSpawn) {
+          if (this.rand(0, 100) == 0) {
+            light.canSpawn = false;
+            this.createL(light.path[pc].x, light.path[pc].y, false);
+          }
+        }
+      }
+      if (!light.hasFired) {
+        this.ctx.fillStyle = 'rgba(255, 255, 255, ' + this.rand(4, 12) / 100 + ')';
+        this.ctx.fillRect(0, 0, this.cw, this.ch);
+      }
+      if (this.rand(0, 30) == 0) {
+        this.ctx.fillStyle = 'rgba(255, 255, 255, ' + this.rand(1, 3) / 100 + ')';
+        this.ctx.fillRect(0, 0, this.cw, this.ch);
+      }
+      this.ctx.stroke();
+    }
+  };
+
+  // Lightning Timer
+  this.lightningTimer = function () {
+    this.lightTimeCurrent++;
+    if (this.lightTimeCurrent >= this.lightTimeTotal) {
+      const newX = this.rand(100, cw - 100);
+      const newY = this.rand(0, ch / 2);
+      let createCount = this.rand(1, 3);
+      while (createCount--) {
+        this.createL(newX, newY, true);
+      }
+      this.lightTimeCurrent = 0;
+      this.lightTimeTotal = this.rand(30, 100);
+    }
+  };
+
+  // Clear Canvas
+  this.clearCanvas = function () {
+    this.ctx.globalCompositeOperation = 'destination-out';
+    this.ctx.fillStyle = 'rgba(0,0,0,' + this.rand(1, 30) / 100 + ')';
+    this.ctx.fillRect(0, 0, this.cw, this.ch);
+    this.ctx.globalCompositeOperation = 'source-over';
+  };
+
+  // Resize on Canvas on Window Resize
+  $(window).on('resize', function () {
+    _this.cw = _this.c.width = _this.c.offsetWidth;
+    _this.ch = _this.c.height = _this.c.offsetHeight;
+  });
+
+  // Animation Loop
+  this.loop = function () {
+    const loopIt = function () {
+      requestAnimationFrame(loopIt, _this.c);
+      _this.clearCanvas();
+      _this.updateL();
+      _this.lightningTimer();
+      _this.renderL();
+    };
+    loopIt();
+  };
 };
 
 /********************************
