4c695dbf16171e3b2d1fe8887b36c9121f107d99
zun5761 <zun5761@gmail.com>
2022-09-26 15:12:53 +0900

    ui-front.js

--- front/js/ui-front.js
+++ front/js/ui-front.js
@@ -1435,7 +1435,7 @@ ui.Button = {
     const $elW = $element.outerWidth();
     const $elH = $element.outerHeight();
     const $min = $elW < $elH ? $elW / 2 : $elH / 2;
-    const $max = $elW < $elH ? $elH : $elW;
+    // const $max = $elW < $elH ? $elH : $elW;
     let $html = '<div class="loading-svg" role="img">';
     $html += '<svg width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">';
     $html += '<circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>';
